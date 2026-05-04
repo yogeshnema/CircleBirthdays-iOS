2934e23cfa6a2f7c21cadd75023acefb201f7692
Yogesh N <nemayogesh@gmail.com>
2026-05-04 12:14:35 +0530

    Initial Commit

---
+++ CircleBirthdays/CircleBirthdaysApp.swift
@@ -0,0 +1,17 @@
+//
+//  CircleBirthdaysApp.swift
+//  CircleBirthdays
+//
+//  Created by Ambika Miya on 04/05/26.
+//
+
+import SwiftUI
+
+@main
+struct CircleBirthdaysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CircleBirthdays/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CircleBirthdays
+//
+//  Created by Ambika Miya on 04/05/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
